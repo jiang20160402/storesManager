0cb87f8befb05d090d6e9512d6dc3ac86c9b4a39
Gurman <778926825@qq.com>
2017-07-20 14:03:05 +0800

    test

---
+++ src/components/main.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+      <router-link to='/orderList'>aaaa</router-link>
+      <router-link to='/three'>one</router-link>
+      <router-link to='/four'>two</router-link>
+      <div>
+        <keep-alive>
+          <router-view></router-view>
+        </keep-alive>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+      computed: {
+        defaultActive: function(){
+          return this.$route.path.replace('/', '');
+        }
+      },
+    }
+</script>
+
+<style>
+
+</style>
